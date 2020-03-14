e59c21b35d7d062f3bc4a6ac57a13a227d49c700
kenjisato0621 <ks01280719@gmail.com>
2020-03-14 16:50:14 -0300

    課題２_crud使用

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users/new', to: 'users#new'
+
+  post '/users', to: 'users#create'
+
+  get '/users', to: 'users#index'
+
+  delete '/users/:id', to: 'users#destroy'
+
+  get '/users/:id/edit', to: 'users#edit'
+
+  patch '/users/:id', to: 'users#update'
+
+end
